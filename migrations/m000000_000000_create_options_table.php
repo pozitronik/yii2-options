de501cb29d9e496105a09c1a3159f1e1a108cb35
Pozitronik <antikillerxxl@gmail.com>
2021-07-21 23:06:00 +0300

    PostgreSQL unique index naming

--- migrations/m000000_000000_create_options_table.php
+++ migrations/m000000_000000_create_options_table.php
@@ -6,24 +6,26 @@ use yii\db\Migration;
  * Class m000000_000000_create_users_options_table
  */
 class m000000_000000_create_options_table extends Migration {
+	private const TABLE_NAME = 'sys_options';
+
 	/**
 	 * {@inheritdoc}
 	 */
 	public function safeUp() {
-		$this->createTable('sys_options', [
+		$this->createTable(self::TABLE_NAME, [
 			'id' => $this->primaryKey(),
 			'option' => $this->string(256)->notNull()->comment('Option name'),
 			'value' => $this->binary()->null()->comment('Serialized option value')
 		]);
 
-		$this->createIndex('option', 'sys_options', ['option'], true);
+		$this->createIndex(self::TABLE_NAME.'_option', self::TABLE_NAME, ['option'], true);
 	}
 
 	/**
 	 * {@inheritdoc}
 	 */
 	public function safeDown() {
-		$this->dropTable('sys_options');
+		$this->dropTable(self::TABLE_NAME);
 	}
 
 }
