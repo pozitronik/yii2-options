15284735e7b5c4584037cb0808d462b1ef12e56e
Pozitronik <antikillerxxl@gmail.com>
2023-04-18 18:23:02 +0400

    codeception framework added

---
+++ tests/Unit/MainTest.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Unit;
+
+use Codeception\Test\Unit;
+use Tests\Support\UnitTester;
+
+/**
+ *
+ */
+class MainTest extends Unit {
+
+	protected UnitTester $tester;
+
+	/**
+	 * @Override
+	 */
+	protected function _before():void {
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testSomeFeature():void {
+
+	}
+}
